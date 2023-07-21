9ddb2cb8527b2e1c29e70de20680a4343750e3e9
danielmiranda0099 <danielmesa877@gmail.com>
2023-07-21 17:36:41 -0500

    [UPGRADE - REMOVE] Update Desing Lesson Card

--- src/components/LessonCard.jsx
+++ src/components/LessonCard.jsx
@@ -1,106 +1,89 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
-import { HEIGHT, themeStyles as theme } from '../styles';
+import { useNavigation } from "@react-navigation/native";
+import { Image, StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
+import { SIZES, themeStyles as theme } from "../styles";
 
-export function LessonCard({title="", textContent="", urlImage, path=null, isFlip=true}) {
+export function LessonCard({
+  title = "",
+  textContent = "",
+  urlImage,
+  path = null,
+  isFlip = false,
+  id,
+}) {
+  const imagen = require("../assets/girl-desk-1.png");
 
+  const navigation = useNavigation();
+  
   const styles = {
     ...stylesInit,
     containerCard: {
       ...stylesInit.containerCard,
-      flexDirection: isFlip ? 'row-reverse' : 'row'
+      flexDirection: isFlip ? "row-reverse" : "row",
     },
-    cardTitle: {
-      ...stylesInit.cardTitle,
-      left: isFlip ? '-20%'  : '-28%',
-    },
-    containerCardBodyInfo: {
-      ...stylesInit.containerCardBodyInfo,
-      borderTopRightRadius: isFlip ? 24 : 0,
-      borderBottomRightRadius: isFlip ? 24 : 0,
-      borderTopLeftRadius: isFlip ? 0 : 24,
-      borderBottomLeftRadius: isFlip ? 0 : 24,
-      paddingLeft: isFlip ? 35: 15,
-    },
-    CardBodyInfoPath: {
-      ...stylesInit.CardBodyInfoPath,
-      right: isFlip ? '50%' : '-25%',
-    }
+  };
+
+  const handleClick = () => {
+    navigation.navigate("Leccion", {id});
   }
 
   return (
-    <View style={styles.containerCard}>
-      <View style={styles.containerCardBodyInfo}>
-        <Text style={styles.cardText}>
-          {textContent}
-        </Text>
-        <View style={styles.CardBodyInfoPath}></View>
-      </View>
+    <TouchableWithoutFeedback onPress={handleClick}>
+      <View style={styles.containerCard}>
+        <View style={styles.containerCardBodyInfo}>
+          <Text style={styles.cardText}>{textContent}</Text>
+        </View>
 
-      <View style={styles.containerCardImage}>
-        <Image style={styles.cardImage} source={urlImage}  />
-        <View style={styles.cardTitle}>
-          <Text style={styles.cardTitleText}>{title}</Text>
+        <View style={styles.containerCardImage}>
+          <Image style={styles.cardImage} source={imagen} />
         </View>
       </View>
-    </View>
-  )
+    </TouchableWithoutFeedback>
+  );
 }
 
 const stylesInit = StyleSheet.create({
   containerCard: {
     display: "flex",
+    justifyContent: 'center',
+    alignItems: 'center',
     width: "95%",
-    height: HEIGHT * 0.14,
-    borderRadius: 24,
-    padding: 5,
-    paddingHorizontal: 10,
-    paddingLeft: 15,
-    // backgroundColor: "white",
+    borderRadius: 100,
+    marginBottom: 80,
+    backgroundColor: "white",
+    borderColor: "#f0b22d",
+    borderWidth: 8,
+    padding: 8,
   },
   containerCardBodyInfo: {
-    width: '65%',
-    position: 'relative',
-    backgroundColor: 'white',
-    paddingHorizontal: 15,
-    paddingVertical: 10,
+    width: "65%",
+    height: (SIZES.medium * 1.1) * 4,
+    paddingLeft: 28,
+    paddingRight: 5,
+    paddingVertical: 0,
+    borderRadius: 100,
+    overflow: 'hidden',
+    display: 'flex',
+    justifyContent: 'center',
+    
   },
   cardText: {
-    ...theme.text2,
+    ...theme.text1,
     lineHeight: 18,
-  },
-  CardBodyInfoPath: {
-    position: 'absolute',
-    backgroundColor: 'white',
-    width: '100%',
-    height: HEIGHT * 0.12,
-    zIndex: -1,
+    textAlign: 'center',
   },
   containerCardImage: {
     flex: 1,
-    position: 'relative',
+    position: "relative",
     // backgroundColor: 'blue',
   },
   cardImage: {
-    width: '110%',
-    height: 'auto',
+    width: "130%",
+    height: "auto",
     aspectRatio: 1 / 1,
-    position: 'absolute',
-    top: '-30%',
+    position: "absolute",
+    // top: "-110%",//sube mucho -95 usar
+    top: "-95%",
+    left: "-15%",
     // backgroundColor: 'red',
-  },
-  cardTitle: {
-    position: 'absolute',
-    bottom: '-30%',
-    width: '150%',
-    padding: 5,
-    borderRadius: 24,
-    // backgroundColor: '#CCEBFF',
-    backgroundColor: '#FFE6E6',
-  },
-  cardTitleText: {
-    ...theme.text2,
-    textAlign: 'center',
-    fontWeight: 'bold',
-    color: "#171717",
-  },
+  }
 });
