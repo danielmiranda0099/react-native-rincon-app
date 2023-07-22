b1a9d391afdc99b3c16b2f209798957bdffeaa62
danielmiranda0099 <danielmesa877@gmail.com>
2023-07-22 15:10:07 -0500

    [ADD] Get Asset By Name

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+export const assets = {
+  "back-girl-beach": require("./back-girl-beach.png"),
+  "corte_pelo": require("./corte_pelo.png"),
+  "girl-desk-1": require("./girl-desk-1.png"),
+  "girl-wich-book-1": require("./girl-wich-book-1.png"),
+}
