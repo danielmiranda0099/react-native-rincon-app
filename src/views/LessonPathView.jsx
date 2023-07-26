14e904f1329cb916e7b152eb4773e65dae0e686f
danielmiranda0099 <danielmesa877@gmail.com>
2023-07-26 18:47:53 -0500

    [ADD - FIX] Generate Unique ID for List Map

--- src/views/LessonPathView.jsx
+++ src/views/LessonPathView.jsx
@@ -1,55 +1,80 @@
-import { Pressable, SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
+import uuid from "react-native-uuid";
+
+import React from "react";
 import { assets } from "../assets";
 import { BackgroundImage, LessonCard } from "../components";
-import { lessons } from "../sections/lessons";
+import { lessonsPath } from "../sections/lessons";
 import { themeStyles as theme } from "../styles";
 
-export function LessonPathView({navigation}) {
-
-
+export function LessonPathView({ navigation }) {
   const handleClickNabar = (path) => {
     navigation.navigate(path);
-  }
-
+  };
+  console.log(uuid.v4());
   return (
     <SafeAreaView>
-    <ScrollView showsVerticalScrollIndicator={false}>
-      
-      <View style={styles.containerLessonPathView}>
-      <View style={styles.containerNavbar}>
-        <Pressable onPress={() => handleClickNabar('Lecciones')}>
-          <Text>Lecciones</Text>
-        </Pressable>
-        <Pressable onPress={() => handleClickNabar('Challenges')}>
-          <Text>Pruebas</Text>
-        </Pressable>
-      </View>
-        <View>
-          <BackgroundImage
-            imageUrl={assets["back-girl-beach"]}
-            styleImage={{ width: "55%", marginTop: 0 }}
-          />
-        </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.containerLessonPathView}>
+          <View style={styles.containerNavbar}>
+            <Pressable onPress={() => handleClickNabar("Lecciones")}>
+              <Text>Lecciones</Text>
+            </Pressable>
+            <Pressable onPress={() => handleClickNabar("Challenges")}>
+              <Text>Pruebas</Text>
+            </Pressable>
+          </View>
+          <View>
+            <BackgroundImage
+              imageUrl={assets["back-girl-beach"]}
+              styleImage={{ width: "55%", marginTop: 0 }}
+            />
+          </View>
 
-        <View>
-          <Text style={styles.header}>BIENVENNID@ A RINCON TEST</Text>
-          <Text style={styles.text}>
-            Aqui encontraras tips, teoria e ideas sobre el cuidado de tu pelo
-            afro y lo mas genial es que podras poner a prueba tus conocimientos
-          </Text>
-        </View>
+          <View>
+            <Text style={styles.header}>BIENVENNID@ A RINCON TEST</Text>
+            <Text style={styles.text}>
+              Aqui encontraras tips, teoria e ideas sobre el cuidado de tu pelo
+              afro y lo mas genial es que podras poner a prueba tus
+              conocimientos
+            </Text>
+          </View>
 
-        {lessons.map((item, index) => (
-          <LessonCard
-            titleCard={item.title}
-            isFlip={index % 2 === 0 ? true : false}
-            id= {item.id}
-            key={index}
-          />
-        ))}
-      </View>
-    </ScrollView>
+          {lessonsPath.map((lessonGroup) => (
+            <React.Fragment key={uuid.v4()}>
+              {lessonGroup.map(
+                (item, index) =>
+                  item.title && (
+                    <LessonCard
+                      titleCard={item.title}
+                      isFlip={index - (1 % 2) === 0 ? true : false}
+                      id={item.id}
+                      key={uuid.v4()}
+                    />
+                  )
+              )}
+              <Pressable
+                onPress={() =>
+                  navigation.navigate("Challenge", {
+                    id: lessonGroup[0].challenge,
+                  })
+                }
+                key={uuid.v4()}
+              >
+                <Text>Pon a pruebas esta leccion</Text>
+              </Pressable>
+            </React.Fragment>
+          ))}
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -61,12 +86,12 @@ const styles = StyleSheet.create({
     backgroundColor: "#A1AFC9",
   },
   containerNavbar: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    width: '100%',
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    width: "100%",
     height: 40,
-    backgroundColor: 'white'
+    backgroundColor: "white",
   },
 });
