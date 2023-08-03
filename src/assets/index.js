504c45b5c0891cee1c08e3ea1086015c974ee4ff
danielmiranda0099 <danielmesa877@gmail.com>
2023-08-02 21:37:10 -0500

    [ADD] Icon Tab Bottom

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,4 +5,8 @@ export const assets = {
   "girl-desk-1": require("./girl-desk-1.png"),
   "girl-wich-book-1": require("./girl-wich-book-1.png"),
   "button-lesson": require("./button-lesson.png"),
+  "icon-active": require("./icon-active.png"),
+  "icon-inactive": require("./icon-inactive.png"),
+  "back-home": require("./back-home.png"),
+  "dec-text-1": require("./dec-text-1.png"),
 }
--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,41 +1,81 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
-import { Text, View } from "react-native";
+import { getFocusedRouteNameFromRoute, useNavigationState } from "@react-navigation/native";
+import { Image, Text, View } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { assets } from "../assets";
 import { HomeView } from "../views";
 import { LessonsNavigation } from "./LessonsNavigation";
 
 const BottomTab = createBottomTabNavigator();
 
 export function Navigation() {
+  const navigationState = useNavigationState((state) => state);
   return (
-    <BottomTab.Navigator>
+    <BottomTab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: "pink",
+      }}
+    >
       <BottomTab.Screen
         name="Inicio"
         component={HomeView}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="home" color={color} size={size} />
+          ),
+        }}
       />
       <BottomTab.Screen
         name="Favoritos"
         component={Componente}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="heart" color={color} size={size} />
+          ),
+        }}
       />
       <BottomTab.Screen
         name="Lecciones"
         component={LessonsNavigation}
         // options={{ headerShown: false, }}
-        options={({ route }) => ({
-          tabBarStyle: ((route) => {
-            const routeName = getFocusedRouteNameFromRoute(route) ?? ""
-            // console.log("*******Route Name: ", getFocusedRouteNameFromRoute(route))
-            if (routeName === 'Challenge' || routeName === 'Leccion') {
-              return { display: "none" }
-            }
-            return
-          })(route),
-          headerShown: false,
-        })}
+        options={({ route }) => {
+          const routeName = getFocusedRouteNameFromRoute(route) ?? "";
+          const isLeccionesRoute = (routeName === "Leccion" || routeName === "Challenge");
+
+          const currentRoute = navigationState?.routes[navigationState.index];
+          const routeMain = currentRoute?.name;
+          const isLeccionesMain = routeMain === "Lecciones";
+
+          return {
+            tabBarStyle: isLeccionesRoute ? { display: "none" } : {},
+            headerShown: false,
+            tabBarIcon: () => (
+              <RenderIcon isFocused={isLeccionesMain}  />
+            ),
+            tabBarActiveBackgroundColor: 'pink',
+            tabBarActiveTintColor: "white",
+          };
+        }}
       />
-      <BottomTab.Screen name="Medallas" component={Componente} />
-      <BottomTab.Screen name="Cuenta" component={Componente} />
+      <BottomTab.Screen
+        name="Medallas"
+        component={Componente}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="trophy" color={color} size={size} />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="Cuenta"
+        component={Componente}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="user" color={color} size={size} />
+          ),
+        }}
+      />
     </BottomTab.Navigator>
   );
 }
@@ -45,5 +85,23 @@ function Componente() {
     <View>
       <Text>Hola este soy yo</Text>
     </View>
+  );
+}
+
+function RenderIcon({isFocused}) {
+  if (isFocused) {
+    return (
+      <Image
+      source={assets["icon-active"]}
+      style={{ width: 35, height: 35, top: 3 }}
+    />
+    );
+  }
+
+  return (
+    <Image
+      source={assets["icon-inactive"]}
+      style={{ width: 35, height: 35, top: 3 }}
+    />
   );
 }
